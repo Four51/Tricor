2ca690e218004b9087722a9de5846aa4d3368b61
Jen Rasmussen <jrasmussen@four51.com>
2015-08-03 11:10:06 -0500

    TRT-01917 Billing Same as shipping address

--- app/js/app.js
+++ app/js/app.js
@@ -2,5 +2,6 @@
 
 var four51 = {};
 four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap',
-    'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-ProductMatrix', 'OrderCloud-ProductLightbox', 'OrderCloud-Mandrill']);
+    'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-ProductMatrix', 'OrderCloud-ProductLightbox', 'OrderCloud-Mandrill',
+'OrderCloud-SameAsShippingCheckbox']);
 
